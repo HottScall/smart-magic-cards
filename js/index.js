f78b219d62f28d2c0e2896edc0339e3eafd7d1d0
Scott <Scotthall1983@hotmail.com>
2019-11-29 14:49:24 +0000

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const suit = 'hearts';
-const cardsWrapper = document.querySelector('.cards-wrapper');
+const suit = "hearts";
+const cardsWrapper = document.querySelector(".cards-wrapper");
 
 function createCards() {
   const cards = [];
@@ -7,7 +7,7 @@ function createCards() {
   for (let i = 1; i <= 13; i += 1) {
     const cardObject = {
       value: i,
-      suit,
+      suit
     };
     cards.push(cardObject);
   }
@@ -15,9 +15,9 @@ function createCards() {
   // For each dataObject, create a new card and append it to the DOM
   cards.forEach((card, i) => {
     const positionFromLeft = i * 15;
-    const cardElement = document.createElement('div');
-    cardElement.setAttribute('data-value', card.value);
-    cardElement.classList.add('card', `${card.suit}-${card.value}`);
+    const cardElement = document.createElement("div");
+    cardElement.setAttribute("data-value", card.value);
+    cardElement.classList.add("card", `${card.suit}-${card.value}`);
     cardElement.style.left = `${positionFromLeft}px`;
     cardsWrapper.append(cardElement);
   });
@@ -35,4 +35,4 @@ function startGame() {
   createCards();
 }
 
-document.getElementById('start-game').addEventListener('click', startGame);
+document.getElementById("start-game").addEventListener("click", startGame);
